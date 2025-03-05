f3efd852d9d1e6401c5a3260f1efa37d220344bb
larry-cm <xetisimo@gmail.com>
2025-03-05 07:32:43 -0500

    acept

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,18 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
-import netlify from '@astrojs/netlify';
+
+import vercel from '@astrojs/vercel';
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
-  adapter: netlify({ edgeMiddleware: true, imageCDN: true, cacheOnDemandPages: true }),
+
   vite: {
     plugins: [tailwindcss()],
   },
 
+  adapter: vercel({}),
 });
