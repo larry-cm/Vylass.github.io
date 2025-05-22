25da119ee4bbad9a00421d4b94708ce1acfb4d44
larry-cm <xetisimo@gmail.com>
2025-05-22 01:59:05 -0500

    no encryptionKey

--- configTurso.js
+++ configTurso.js
@@ -3,5 +3,4 @@ import { createClient } from "@libsql/client";
 export const turso = createClient({
     url: import.meta.env.SECRET_ASTRO_DB_REMOTE_URL,
     authToken: import.meta.env.SECRET_ASTRO_DB_APP_TOKEN,
-    encryptionKey: import.meta.env.SECRET_ASTRO_DB_ENCRYPTION_KEY,
 });
