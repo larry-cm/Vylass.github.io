2fafdb0540d2ea7d21733aa858072a92037ca988
larry-cm <xetisimo@gmail.com>
2025-06-14 18:43:13 -0500

    style: :art: dandole una mejor vista a las cards.

--- src/components/ButtonMenu.tsx
+++ src/components/ButtonMenu.tsx
@@ -1,26 +1,26 @@
-import { IconDotsMenu, IconGo } from "@/assets/Icons"
+import { IconAccessibility, IconDotsMenu, IconDownload, IconGo } from "@/assets/Icons"
 import { useState } from "react"
 
-function DropMenu(handleClose: () => void) {
+function DropMenu(handleClose: () => void, urls: string[]) {
     return (
-        <ul onMouseLeave={handleClose} className="bg-neutral-800/90 backdrop-blur-md w-fit p-4 rounded-md absolute top-18 space-y-2 z-20">
-            <li className="rounded hover:bg-neutral-700/80 py-2 px-3 min-w-44 text-start w-fit transition flex items-center h-fit ">Ver Ahora <IconGo className='size-5 scale-105 flex items-center ms-2 mt-0.5' /></li>
-            <li className="rounded hover:bg-neutral-700/80 py-2 px-3 min-w-44 text-start w-fit transition flex items-center h-fit ">Mejorar</li>
-            <li className="rounded hover:bg-neutral-700/80 py-2 px-3 min-w-44 text-start w-fit transition flex items-center h-fit ">Descargar</li>
-        </ul>
+        <menu onMouseLeave={handleClose} className="bg-neutral-800/90 backdrop-blur-md w-fit p-4 rounded-md absolute top-18 space-y-2 z-20 text-normal">
+            <a href={urls[0]} className="rounded hover:bg-neutral-700/50 py-2 px-3 hover:-translate-y-0.5 hover:shadow-md min-w-44 text-start w-fit transition-all duration-300 flex items-center h-fit "> <IconGo className='size-5 scale-105 flex items-center me-2 mt-0.5' />Ver Ahora </a>
+            <a href={urls[1]} className="rounded hover:bg-neutral-700/50 py-2 px-3 hover:-translate-y-0.5 hover:shadow-md min-w-44 text-start w-fit transition-all duration-300 flex items-center h-fit "><IconAccessibility className='size-6  flex items-center me-2 ' /> Mejorar</a>
+            <a href={urls[2]} className="rounded hover:bg-neutral-700/50 py-2 px-3 hover:-translate-y-0.5 hover:shadow-md min-w-44 text-start w-fit transition-all duration-300 flex items-center h-fit "><IconDownload className='size-5 scale-105 flex items-center me-2 ' /> Descargar</a>
+        </menu>
     )
 }
-export default function ButtonMenu() {
+export default function ButtonMenu({ urls }: { urls: string[] }) {
     const [openMenu, setOpenMenu] = useState(false)
+    const [x, y, z] = urls
     const handleClose = () => {
         setOpenMenu(e => !e)
     }
     return (
         <>
             <button
-                onMouseLeave={handleClose}
                 onClick={handleClose}
-                className="animate-fade-in bg-neutral-800/80 hover:bg-neutral-800 rounded p-2 transition"
+                className="animate-fade-in bg-neutral-800/80 hover:bg-neutral-700/50 rounded p-2 transition"
             >
                 <IconDotsMenu
                     className="size-6 min-w-6 text-slate-200"
@@ -28,7 +28,7 @@ export default function ButtonMenu() {
 
             </button >
             {
-                openMenu && DropMenu(handleClose)
+                openMenu && DropMenu(handleClose, [x, y, z])
             }
         </>
     )
